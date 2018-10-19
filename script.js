16ed38391004ac10c7b26bea25fdf6f5b7be6cde
Xavier <43177673+zashurazxz@users.noreply.github.com>
2018-10-18 22:59:11 -0600

    prepped for class

--- script.js
+++ script.js
@@ -40,6 +40,51 @@ $menuLi.on("click", function(event) {
     event.preventDefault();
 });});
 
+
+let contactinfo = [
+    agentNames= ["george", "bob", "ken", "alex"],
+    Phonenumbers= ["1231111111","1232222222", "1233333333", "1234444444"]
+];
+function makeUL(array) {
+    // Create the list element:
+    let list = document.createElement("ul");
+    for(let i = 0; i < array.length; i++) {
+        // Create the list item:
+        let item = document.createElement("li");
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i]));
+        // Add it to the list:
+        list.appendChild(item);
+    }
+    // Finally, return the constructed list:
+    return list;
+}
+// Add the contents of contactinfo[0] to #agents:
+document.getElementById("agents").append(makeUL(contactinfo[0]));
+document.getElementById("agents").appendChild(makeUL(contactinfo[1]));
+
+
+
+// this is the enter leave code that swaps the image
+
+var sourceSwap = function () {
+    var $this = $(this);
+    var newSource = $this.data('alt-src');
+    $this.data('alt-src', $this.attr('src'));
+    $this.attr('src', newSource);
+}
+$(function() {
+    $('img[data-alt-src]').each(function() { 
+        new Image().src = $(this).data('alt-src'); 
+    }).hover(sourceSwap, sourceSwap); 
+});
+
+
+
+
+
+
+// footer clock
 document.getElementById("footing").innerHTML = formatAMPM();
 function formatAMPM() {
 var d = new Date(),
@@ -47,5 +92,3 @@ var d = new Date(),
     days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+'th '+d.getFullYear();
 }
-
-
